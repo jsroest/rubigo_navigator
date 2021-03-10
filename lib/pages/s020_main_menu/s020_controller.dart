9238d168520f7c82988e5919cabfa8173c75eaa0
Sander Roest <s.roest@dalosy.com>
2021-03-10 11:02:25 +0100

    Go from page 'b' directly to 'c'

--- lib/pages/s020_main_menu/s020_controller.dart
+++ lib/pages/s020_main_menu/s020_controller.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_rubigo_navigator/navigator/rubigo_controller.dart';
 import 'package:flutter_rubigo_navigator/navigator/rubigo_material_page.dart';
 import 'package:flutter_rubigo_navigator/navigator/rubigo_navigator.dart';
+import 'package:flutter_rubigo_navigator/navigator/rubigo_stack_manager.dart';
 import 'package:flutter_rubigo_navigator/pages/s020_main_menu/s020_main_menu_page.dart';
 
 final s020ControllerProvider = ChangeNotifierProvider<S020Controller>(
@@ -16,6 +19,19 @@ final s020ControllerProvider = ChangeNotifierProvider<S020Controller>(
 class S020Controller extends RubigoController {
   S020Controller(RubigoMaterialPage page, RubigoNavigator rubigoNavigator)
       : super(page, rubigoNavigator);
+
+  @override
+  FutureOr<void> onTop(
+    StackChange stackChange,
+    RubigoController previousController,
+  ) {
+    if (stackChange == StackChange.pushed_on_top) {
+      rubigoNavigator.push('c');
+    }
+  }
+
+  @override
+  FutureOr<void> isShown() {}
 
   void doContinue() {
     rubigoNavigator.push('c');
