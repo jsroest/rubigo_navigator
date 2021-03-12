4ee6b3ca7c59af68243dde950417e4f0e4cd7f13
Sander Roest <s.roest@dalosy.com>
2021-03-12 11:12:21 +0100

    Fix rubigo_page brick

--- packages/app/bricks/rubigo_page/__brick__/lib/pages/{{#snakeCase}}{{page_id}}_{{page_name}}{{/snakeCase}}/{{#snakeCase}}{{page_id}}_{{page_name}}{{/snakeCase}}_page.dart
+++ packages/app/bricks/rubigo_page/__brick__/lib/pages/{{#snakeCase}}{{page_id}}_{{page_name}}{{/snakeCase}}/{{#snakeCase}}{{page_id}}_{{page_name}}{{/snakeCase}}_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/all.dart';
 import 'package:flutter_rubigo_navigator/pages/page_enum.dart';
-import 'package:flutter_rubigo_navigator/pages/{{#snakeCase}}{{page_id}}{{/snakeCase}}_{{#snakeCase}}{{page_name}}{{/snakeCase}}/{{#snakeCase}}{{page_id}}{{/snakeCase}}_controller.dart';
+import 'package:flutter_rubigo_navigator/pages/{{#snakeCase}}{{page_id}}_{{page_name}}{{/snakeCase}}/{{#snakeCase}}{{page_id}}{{/snakeCase}}_controller.dart';
 import 'package:rubigo_navigator/rubigo.dart';
 
 class {{#pascalCase}}{{page_id}}{{/pascalCase}}{{#pascalCase}}{{page_name}}{{/pascalCase}}Page extends RubigoPage<Pages, {{#pascalCase}}{{page_id}}{{/pascalCase}}Controller> {
--- packages/app/bricks/rubigo_page/__brick__/lib/pages/{{#snakeCase}}{{page_id}}_{{page_name}}{{/snakeCase}}/{{#snakeCase}}{{page_id}}{{/snakeCase}}_controller.dart
+++ packages/app/bricks/rubigo_page/__brick__/lib/pages/{{#snakeCase}}{{page_id}}_{{page_name}}{{/snakeCase}}/{{#snakeCase}}{{page_id}}{{/snakeCase}}_controller.dart
@@ -2,13 +2,13 @@ import 'dart:async';
 
 import 'package:flutter_riverpod/all.dart';
 import 'package:flutter_rubigo_navigator/pages/page_enum.dart';
-import 'package:flutter_rubigo_navigator/pages/{{#snakeCase}}{{page_id}}_{{/snakeCase}}_{{page_name}}/{{#snakeCase}}{{page_id}}_{{/snakeCase}}_{{page_name}}_page.dart';
+import 'package:flutter_rubigo_navigator/pages/{{#snakeCase}}{{page_id}}__{{page_name}}{{/snakeCase}}/{{#snakeCase}}{{page_id}}_{{page_name}}{{/snakeCase}}_page.dart';
 import 'package:rubigo_navigator/rubigo.dart';
 
-final {{#pascalCase}}{{page_id}}{{/pascalCase}}ControllerProvider = ChangeNotifierProvider<{{#pascalCase}}{{page_id}}{{/pascalCase}}Controller>(
+final {{#camelCase}}{{page_id}}{{/camelCase}}ControllerProvider = ChangeNotifierProvider<{{#pascalCase}}{{page_id}}{{/pascalCase}}Controller>(
   (ref) {
     return {{#pascalCase}}{{page_id}}{{/pascalCase}}Controller(
-      () => {{#pascalCase}}{{page_id}}{{/pascalCase}}{{#pascalCase}}{{page_name}}{{/pascalCase}}Page({{#pascalCase}}{{page_id}}{{/pascalCase}}ControllerProvider),
+      () => {{#pascalCase}}{{page_id}}{{/pascalCase}}{{#pascalCase}}{{page_name}}{{/pascalCase}}Page({{#camelCase}}{{page_id}}{{/camelCase}}ControllerProvider),
     );
   },
 );
