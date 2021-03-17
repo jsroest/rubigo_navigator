1efb4feed2d6f98b64c4adfca0cc56c88a7e6fb0
Sander Roest <s.roest@dalosy.com>
2021-03-17 16:05:25 +0100

    [rubigo_navigator_app] Enable NNBD for source files that have no changes

--- packages/app/lib/services/ws02_set_quality_control_item.dart
+++ packages/app/lib/services/ws02_set_quality_control_item.dart
@@ -1,5 +1,3 @@
-// @dart=2.9
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_rubigo_navigator/services/ws01_get_quality_control_items.dart';
 import 'package:rubigo_navigator/services/rubigo_busy_service.dart';
