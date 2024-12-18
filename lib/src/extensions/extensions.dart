cf9dd138ecdb77036305913b8ee4c7b81439fd6a
Sander Roest <s.roest@dalosy.com>
2024-12-18 15:47:50 +0100

    Rename FindByScreenId to ExtensionOnListOfRubigoScreens

--- lib/src/extensions/extensions.dart
+++ lib/src/extensions/extensions.dart
@@ -2,7 +2,8 @@ import 'package:flutter/widgets.dart';
 import 'package:rubigo_navigator/src/rubigo_controller.dart';
 import 'package:rubigo_navigator/src/types/rubigo_type_definitions.dart';
 
-extension FindByScreenId<SCREEN_ID extends Enum> on ListOfRubigoScreens {
+extension ExtensionOnListOfRubigoScreens<SCREEN_ID extends Enum>
+    on ListOfRubigoScreens {
   RubigoController<SCREEN_ID> findController(SCREEN_ID screenId) =>
       firstWhere((e) => e.screenId == screenId).controller
           as RubigoController<SCREEN_ID>;
