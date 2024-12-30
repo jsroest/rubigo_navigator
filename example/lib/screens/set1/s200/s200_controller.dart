afff825f7005410197dca9eebaba0116f09a722d
Sander Roest <s.roest@dalosy.com>
2024-12-30 21:49:47 +0100

    Info about delay in S200

--- example/lib/screens/set1/s200/s200_controller.dart
+++ example/lib/screens/set1/s200/s200_controller.dart
@@ -5,8 +5,12 @@ import 'package:rubigo_navigator/rubigo_navigator.dart';
 class S200Controller with RubigoController<Screens> {
   @override
   Future<void> onTop(RubigoChangeInfo<Screens?> changeInfo) async {
-    //Simulate slow backend
-    await Future<void>.delayed(const Duration(seconds: 2));
+    if (changeInfo.stackChange == StackChange.isPushed) {
+      // Simulate slow backend, demonstrate we are not able to push any buttons
+      // while this is in progress. As opposed of consumer apps this is a
+      // perfect fit for Line of Business apps
+      await Future<void>.delayed(const Duration(seconds: 2));
+    }
   }
 
   Future<void> onS300ButtonPressed() async {
