d81f286a8d330da57fe2566c335349007abcb715
Sander Roest <s.roest@dalosy.com>
2025-01-07 13:27:01 +0100

    Change to non-nullable type

--- lib/src/rubigo_controller.dart
+++ lib/src/rubigo_controller.dart
@@ -3,11 +3,11 @@ import 'package:rubigo_navigator/src/rubigo_router.dart';
 mixin RubigoController<SCREEN_ID extends Enum> {
   late RubigoRouter<SCREEN_ID> rubigoRouter;
 
-  Future<void> onTop(RubigoChangeInfo<SCREEN_ID?> changeInfo) async {}
+  Future<void> onTop(RubigoChangeInfo<SCREEN_ID> changeInfo) async {}
 
-  Future<void> willShow(RubigoChangeInfo<SCREEN_ID?> changeInfo) async {}
+  Future<void> willShow(RubigoChangeInfo<SCREEN_ID> changeInfo) async {}
 
-  Future<void> isShown(RubigoChangeInfo<SCREEN_ID?> changeInfo) async {}
+  Future<void> isShown(RubigoChangeInfo<SCREEN_ID> changeInfo) async {}
 
   Future<bool> mayPop() => Future.value(true);
 }
