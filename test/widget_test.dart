d9146793e9f66e1b6fd2dc152bd51e0049701d30
Sander Roest <s.roest@dalosy.com>
2025-01-07 22:12:26 +0100

    Add a test for both onDidRemovePage and onPopPage.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -17,55 +17,76 @@ void main() {
       rubigoNavigatorObserver = RubigoNavigatorObserver<Screens>();
     },
   );
-  testWidgets(
-    'test splashWidget => S100',
-    (tester) async {
-      final holder = RubigoControllerHolder<MockController<Screens>>();
-      final availableScreens = [
-        getSplashScreen(holder),
-        getS100Screen(holder),
-        getS200Screen(holder),
-        getS300Screen(holder),
-        getS500Screen(holder),
-        getS600Screen(holder),
-        getS700Screen(holder),
-      ];
-      await tester.pumpWidget(
-        RubigoMaterialApp(
-          routerDelegate: RubigoRouterDelegate(
-            observers: [
-              mockNavigatorObserver,
-              rubigoNavigatorObserver,
-            ],
-            rubigoRouter: RubigoRouter(
-              availableScreens: availableScreens,
-              splashScreenId: Screens.splashScreen,
-            ),
-          ),
-          initAndGetFirstScreen: () async {
-            await Future<void>.delayed(const Duration(milliseconds: 200));
-            return Screens.s100;
-          },
+
+  Future<void> _widgetTest(
+      WidgetTester tester, BackCallback backCallback) async {
+    final holder = RubigoControllerHolder<MockController<Screens>>();
+    final availableScreens = [
+      getSplashScreen(holder),
+      getS100Screen(holder),
+      getS200Screen(holder),
+      getS300Screen(holder),
+      getS500Screen(holder),
+      getS600Screen(holder),
+      getS700Screen(holder),
+    ];
+    final rubigoRouter = RubigoRouter(
+      availableScreens: availableScreens,
+      splashScreenId: Screens.splashScreen,
+    );
+    await tester.pumpWidget(
+      RubigoMaterialApp(
+        routerDelegate: RubigoRouterDelegate(
+          observers: [
+            mockNavigatorObserver,
+            rubigoNavigatorObserver,
+          ],
+          rubigoRouter: rubigoRouter,
+          backCallback: backCallback,
         ),
-      );
-      await tester.pumpAndSettle();
-      verifyInOrder(
-        [
-          mockNavigatorObserver.navigator,
-          mockNavigatorObserver.didPush(any, any),
-          mockNavigatorObserver.didChangeTop(any, any),
-        ],
-      );
-      expect(rubigoNavigatorObserver.currentScreenId, Screens.splashScreen);
-      await tester.pumpAndSettle(const Duration(milliseconds: 200));
-      verifyInOrder(
-        [
-          mockNavigatorObserver.didPush(any, any),
-          mockNavigatorObserver.didRemove(any, any),
-          mockNavigatorObserver.didChangeTop(any, any),
-        ],
-      );
-      expect(rubigoNavigatorObserver.currentScreenId, Screens.s100);
+        initAndGetFirstScreen: () async {
+          await Future<void>.delayed(const Duration(milliseconds: 200));
+          return Screens.s100;
+        },
+      ),
+    );
+    await tester.pumpAndSettle();
+    verifyInOrder(
+      [
+        mockNavigatorObserver.navigator,
+        mockNavigatorObserver.didPush(any, any),
+        mockNavigatorObserver.didChangeTop(any, any),
+      ],
+    );
+    expect(rubigoNavigatorObserver.currentScreenId, Screens.splashScreen);
+    await tester.pumpAndSettle(const Duration(milliseconds: 200));
+    verifyInOrder(
+      [
+        mockNavigatorObserver.didPush(any, any),
+        mockNavigatorObserver.didRemove(any, any),
+        mockNavigatorObserver.didChangeTop(any, any),
+      ],
+    );
+    expect(rubigoNavigatorObserver.currentScreenId, Screens.s100);
+    await tester.runAsync(() => rubigoRouter.push(Screens.s200));
+    await tester.pumpAndSettle();
+    expect(rubigoNavigatorObserver.currentScreenId, Screens.s200);
+    await tester.pageBack();
+    await tester.pumpAndSettle();
+    expect(rubigoNavigatorObserver.currentScreenId, Screens.s100);
+  }
+
+  testWidgets(
+    'test onPopPage SplashWidget S100 S200 pop',
+    (tester) async {
+      await _widgetTest(tester, BackCallback.onPopPage);
+    },
+  );
+
+  testWidgets(
+    'test onDidRemovePage SplashWidget S100 S200 pop',
+    (tester) async {
+      await _widgetTest(tester, BackCallback.onDidRemovePage);
     },
   );
 }
