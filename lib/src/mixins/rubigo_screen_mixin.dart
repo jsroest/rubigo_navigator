777f316a7a01b843e62d265903442b66266e3947
Sander Roest <s.roest@dalosy.com>
2025-01-09 20:26:35 +0100

    Add documentation.

--- lib/src/mixins/rubigo_screen_mixin.dart
+++ lib/src/mixins/rubigo_screen_mixin.dart
@@ -1,5 +1,9 @@
-import 'package:rubigo_navigator/src/rubigo_controller.dart';
+import 'package:rubigo_navigator/rubigo_navigator.dart';
 
+/// Apply this mixin for easy access to the corresponding controller.
+/// This is wired up during [RubigoRouter.init] for each [RubigoScreen] in the
+/// list of [RubigoRouter.availableScreens]
 mixin RubigoScreenMixin<RUBIGO_CONTROLLER extends RubigoController> {
+  /// The controller that belongs to this screen
   late final RUBIGO_CONTROLLER controller;
 }
