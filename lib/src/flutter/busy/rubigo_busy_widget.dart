1f61ecc019ebd38d557e48e221b629b79d5c6ad6
Sander Roest <s.roest@dalosy.com>
2025-01-09 17:07:58 +0100

    Add documentation and remove unused properties.

--- lib/src/flutter/busy/rubigo_busy_widget.dart
+++ lib/src/flutter/busy/rubigo_busy_widget.dart
@@ -1,23 +1,41 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:rubigo_navigator/rubigo_navigator.dart';
 import 'package:rubigo_navigator/src/flutter/busy/rubigo_busy_event.dart';
 
+/// This widget protect its children by any screen user interaction
+/// by placing an [IgnorePointer] above them in a stack.
+/// ```dart
+/// MaterialApp.router(
+///   backButtonDispatcher: RootBackButtonDispatcher(),
+///   routerDelegate: widget.routerDelegate,
+///   builder: (context, child) {
+///     return RubigoBusyWidget(
+///       progressIndicator: widget.progressIndicator,
+///       listener: widget.routerDelegate.rubigoRouter.rubigoBusy.notifier,
+///       child: child!,
+///     );
+///   },
+/// );
+/// ```
 class RubigoBusyWidget extends StatelessWidget {
+  /// Create RubigoBusyWidget.
   const RubigoBusyWidget({
     required this.child,
     required this.listener,
-    this.progressIndicator,
-    this.opacityWithoutProgressIndicator = 0.0,
-    this.opacityWithProgressIndicator = 0.3,
-    this.barrierColor = Colors.grey,
+    Widget? progressIndicator,
     super.key,
-  });
+  }) : _progressIndicator = progressIndicator ??
+            const Center(
+              child: CircularProgressIndicator(),
+            );
 
+  ///The rest of the app is represented by this child
   final Widget child;
-  final Widget? progressIndicator;
-  final double opacityWithoutProgressIndicator;
-  final double opacityWithProgressIndicator;
-  final Color barrierColor;
+
+  final Widget _progressIndicator;
+
+  /// Connect a [RubigoBusyService.notifier] to this listener.
   final ValueListenable<RubigoBusyEvent> listener;
 
   @override
@@ -31,18 +49,8 @@ class RubigoBusyWidget extends StatelessWidget {
               ignoring: value.enabled && value.isBusy,
               child: child,
             ),
-            if (value.enabled &&
-                value.isBusy &&
-                value.showProgressIndicator &&
-                (progressIndicator == null))
-              const Center(
-                child: CircularProgressIndicator(),
-              ),
-            if (value.enabled &&
-                value.isBusy &&
-                value.showProgressIndicator &&
-                (progressIndicator != null))
-              progressIndicator!,
+            if (value.enabled && value.isBusy && value.showProgressIndicator)
+              _progressIndicator,
           ],
         );
       },
