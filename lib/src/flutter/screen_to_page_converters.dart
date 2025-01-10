096da4085b07fd5a737f33e7ec18b23c61a43142
Sander Roest <s.roest@dalosy.com>
2025-01-10 09:54:13 +0100

    Add documentation.

--- lib/src/flutter/screen_to_page_converters.dart
+++ lib/src/flutter/screen_to_page_converters.dart
@@ -2,12 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rubigo_navigator/src/rubigo_screen.dart';
 
+/// Extensions on [RubigoScreen]
 extension ExtensionOnRubigoScreen on RubigoScreen {
+  /// Converts a [RubigoScreen] to a [MaterialPage]
   MaterialPage<void> toMaterialPage() => MaterialPage(
         key: pageKey,
         child: screenWidget,
       );
 
+  /// Converts a [RubigoScreen] to a [CupertinoPage]
   CupertinoPage<void> toCupertinoPage() => CupertinoPage(
         key: pageKey,
         child: screenWidget,
