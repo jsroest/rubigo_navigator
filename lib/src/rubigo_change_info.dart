ce014abb20f8bde4be550df47b5a8bf96b2a828d
Sander Roest <s.roest@dalosy.com>
2025-01-10 10:29:31 +0100

    Add documentation.

--- lib/src/rubigo_change_info.dart
+++ lib/src/rubigo_change_info.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/foundation.dart';
 
+/// The cause of this navigation event
 enum EventType {
+  /// This screen is on-top because of a push
   push,
+
+  /// This screen is on-top because of a pop
   pop,
+
+  /// This screen is on-top because of a popTo
   popTo,
+
+  /// This screen is on-top because of a replaceStack
   replaceStack,
 }
 
+/// This class contains information about the navigation event class RubigoChangeInfo<SCREEN_ID extends Enum> {
 @immutable
 class RubigoChangeInfo<SCREEN_ID extends Enum> {
+  /// Creates a RubigoChangeInfo
   const RubigoChangeInfo(
     this.eventType,
     this.previousScreen,
     this.screenStack,
   );
 
+  /// The reason why this screen is on-top.
   final EventType eventType;
+
+  /// The screen that was navigated from.
   final SCREEN_ID? previousScreen;
+
+  /// The current screenStack.
   final List<SCREEN_ID> screenStack;
 
   @override
