7467eda06c63517880003d271dbf127c429f621c
Sander Roest <s.roest@dalosy.com>
2025-01-28 14:59:42 +0100

    restore deleted files

---
+++ test/rubigo_material_app/rubigo_router_observer.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateNiceMocks([MockSpec<NavigatorObserver>()])
+class RubigoNavigatorObserver<SCREEN_ID extends Enum>
+    extends NavigatorObserver {
+  late SCREEN_ID? currentScreenId;
+
+  @override
+  void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
+    super.didPush(route, previousRoute);
+    currentScreenId = routeToKey(route);
+  }
+
+  @override
+  void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) {
+    super.didPop(route, previousRoute);
+    currentScreenId = routeToKey(previousRoute!);
+  }
+
+  SCREEN_ID? routeToKey(Route<dynamic> route) {
+    if (route.settings is MaterialPage) {
+      final settings = route.settings as MaterialPage;
+      final key = settings.key! as ValueKey<SCREEN_ID>;
+      return key.value;
+    }
+    return null;
+  }
+}
