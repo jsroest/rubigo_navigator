ccac3aeef6a3b69ef45b3362d5786ddddfb57256
Sander Roest <s.roest@dalosy.com>
2025-01-29 10:01:44 +0100

    Fix doc

--- lib/src/rubigo_router/stack_manager/last_page_popped_exception.dart
+++ lib/src/rubigo_router/stack_manager/last_page_popped_exception.dart
@@ -1,7 +1,4 @@
-import 'package:rubigo_router/rubigo_router.dart';
-
 /// This exception is thrown when the last page is popped from the stack.
-/// It's used by [RubigoPopScope] to close the app on Android.
 class LastPagePoppedException implements Exception {
   /// Creates a LastPagePoppedException
   LastPagePoppedException(this.message);
