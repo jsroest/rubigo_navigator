bd5f48ee644c00876115dbee5b40d717b4c275a8
Sander Roest <s.roest@dalosy.com>
2025-01-31 14:32:41 +0100

    Make private to avoid confusion

--- test/rubigo_holder_test.dart
+++ test/rubigo_holder_test.dart
@@ -6,39 +6,41 @@ void main() {
     'RubigoHolder',
     () {
       final holder = RubigoHolder();
-      final controller1 = holder.getOrCreate(ControllerScreens1.new);
-      final controller2 = ControllerScreens2();
+      final controller1 = holder.getOrCreate(_ControllerScreens1.new);
+      final controller2 = _ControllerScreens2();
       holder.add(controller2);
 
       expect(controller1 is RubigoControllerMixin, true);
-      expect(controller1 is RubigoControllerMixin<Screens>, true);
-      expect(controller1 is ControllerScreens1, true);
-      final tmp1 = holder.getOrCreate(ControllerScreens1.new);
+      expect(controller1 is RubigoControllerMixin<_Screens>, true);
+      expect(controller1 is _ControllerScreens1, true);
+      final tmp1 = holder.getOrCreate(_ControllerScreens1.new);
       expect(identical(controller1, tmp1), true);
 
-      final tmp2 = holder.getOrCreate(ControllerScreens2.new);
+      final tmp2 = holder.getOrCreate(_ControllerScreens2.new);
       expect(identical(controller2, tmp2), true);
 
       expect(identical(controller1, controller2), false);
 
-      final newController1 = ControllerScreens1();
+      final newController1 = _ControllerScreens1();
       holder.add(newController1);
-      final tmp3 = holder.get<ControllerScreens1>();
+      final tmp3 = holder.get<_ControllerScreens1>();
 
       expect(identical(newController1, tmp3), true);
 
-      final tmp4 = holder.getOrCreate(ControllerScreens1.new);
+      final tmp4 = holder.getOrCreate(_ControllerScreens1.new);
 
       expect(identical(tmp3, tmp4), true);
     },
   );
 }
 
-enum Screens {
+enum _Screens {
+  //ignore: unused_field
   screen1,
+  //ignore: unused_field
   screen2,
 }
 
-class ControllerScreens1 with RubigoControllerMixin<Screens> {}
+class _ControllerScreens1 with RubigoControllerMixin<_Screens> {}
 
-class ControllerScreens2 with RubigoControllerMixin<Screens> {}
+class _ControllerScreens2 with RubigoControllerMixin<_Screens> {}
